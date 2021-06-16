a2206326e824d74cdf69464c6b6ae224301f1fa7
feels-good <65146067+feels-good@users.noreply.github.com>
2021-06-16 16:29:41 +0800

    asdda

---
+++ mylibrary/src/main/java/com/fzj/mylibrary/Utils.java
@@ -0,0 +1,10 @@
+package com.fzj.mylibrary;
+
+import android.util.Log;
+
+public class Utils {
+
+    public static void print() {
+        Log.e("hhhhhhhhhhhhhhhh", "hhhhhhhhhhhhhhhhhhh");
+    }
+}
